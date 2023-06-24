c71af435437c60604358908550fd23f17d52803d
Neil Connolly <neil@dequ.co.uk>
2023-06-24 21:22:57 +0100

    wrong clone command for packer sorted!

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 	-- install the packer.nvim plugin using...
-	-- git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+	-- git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
